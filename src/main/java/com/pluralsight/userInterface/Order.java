fe4f991798aa03a323b5aa70b7e88ad5d972b015
arsenii <arseniy.kunilovskiy@gmail.com>
2025-11-07 13:33:08 -0500

    Order class is done

---
+++ src/main/java/com/pluralsight/userInterface/Order.java
@@ -0,0 +1,38 @@
+package com.pluralsight.userInterface;
+
+import com.pluralsight.items.Item;
+
+import java.util.ArrayList;
+
+public class Order {
+    private  int id;
+    private ArrayList<Item> items;
+
+    public void printOrder(){
+
+    }
+
+    public Order(int id, ArrayList<Item> items) {
+        this.id = id;
+        this.items = items;
+    }
+
+    public Order() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+}
